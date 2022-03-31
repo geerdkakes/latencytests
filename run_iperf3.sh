93f40658535dd45a73f0a9778cfa3ead97eceea7
Geerd Kakes <geerd@kakes.net>
2022-03-31 15:36:06 +0200

    changed iperf reporting to 10 seconds

--- run_iperf3.sh
+++ run_iperf3.sh
@@ -120,11 +120,11 @@ ssh ${userid_device}@${deviceIP} "/usr/bin/mkdir -p ${data_dir_device}/${session
 #  -u                   : specify to test using udp
 ######################################################################
 echo "${scriptname}: starting serverside iperf test"
-echo "${scriptname}: ${iperf3_app} -s -1 -J  -p ${iperf3_port} -i 60 > ${data_dir_server}/${session_id}/server_iperf3_${testdate}.json"
+echo "${scriptname}: ${iperf3_app} -s -1 -J  -p ${iperf3_port} -i 10 > ${data_dir_server}/${session_id}/server_iperf3_${testdate}.json"
 result=1
 
 while [ ${result} -eq 1 ]; do
-    ${iperf3_app} -s -1 -J -p ${iperf3_port} -i 60 > ${data_dir_server}/${session_id}/server_iperf3_${testdate}.json &
+    ${iperf3_app} -s -1 -J -p ${iperf3_port} -i 10 > ${data_dir_server}/${session_id}/server_iperf3_${testdate}.json &
     iperf_PID=$!
     sleep 1
     kill -0 $iperf_PID
@@ -138,8 +138,8 @@ while [ ${result} -eq 1 ]; do
 done
 
 echo "${scriptname}: running client side iperf test with MTU: ${MTU} MSS window: ${MSS} Datalenth: ${DATALENGTH} serverIP ${serverIP} and port ${iperf3_port}"
-echo "${scriptname}: ssh ${userid_device}@${deviceIP} \"${iperf3_app} -p ${iperf3_port} -c ${serverIP} -l ${DATALENGTH} -N -t ${time} -T ${session_id} -O 1 -J ${dir_var} -i 60 -P ${streams} ${bitrateoption} ${udp_tcp_specific} > ${data_dir_device}/${session_id}/device_iperf3_${testdate}.json\""
-ssh ${userid_device}@${deviceIP} "${iperf3_app} -c ${serverIP} -p ${iperf3_port} -l ${DATALENGTH} -N -t ${time} -T ${session_id} -O 1 -J ${dir_var} -i 60 -P ${streams} ${bitrateoption} ${udp_tcp_specific} > ${data_dir_device}/${session_id}/device_iperf3_${testdate}.json"
+echo "${scriptname}: ssh ${userid_device}@${deviceIP} \"${iperf3_app} -p ${iperf3_port} -c ${serverIP} -l ${DATALENGTH} -N -t ${time} -T ${session_id} -O 1 -J ${dir_var} -i 10 -P ${streams} ${bitrateoption} ${udp_tcp_specific} > ${data_dir_device}/${session_id}/device_iperf3_${testdate}.json\""
+ssh ${userid_device}@${deviceIP} "${iperf3_app} -c ${serverIP} -p ${iperf3_port} -l ${DATALENGTH} -N -t ${time} -T ${session_id} -O 1 -J ${dir_var} -i 10 -P ${streams} ${bitrateoption} ${udp_tcp_specific} > ${data_dir_device}/${session_id}/device_iperf3_${testdate}.json"
 if [ ${?} -eq 1 ]; then
     echo "${scriptname}:  Error while connecting from client site to iperf3 server"
 else
