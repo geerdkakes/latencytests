7ff08de2ba01f18d16c46edbe16298d76227cc84
Geerd Kakes <geerd@kakes.net>
2022-04-21 22:21:57 +0200

    corrected bug preventing download of iperf3 device json

--- run_iperf3.sh
+++ run_iperf3.sh
@@ -162,5 +162,5 @@ fi
 ##########################################
 # retrieve device logfile 
 ##########################################
-scp ${userid_device}@${deviceIP}:${data_dir_device}/${session_id}/device_iperf3_${testdate}.json  ${data_dir_server}/${session_id}/
+scp ${userid_device}@${deviceIP}:${data_dir_device}/${session_id}/device${testID}_iperf3_${testdate}.json  ${data_dir_server}/${session_id}/
 echo "${scriptname}: stored iperf3 output at ${data_dir_server}/${session_id}/device${testID}_iperf3_${testdate}.json"
