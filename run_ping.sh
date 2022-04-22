615ddd94896ced25c405acc50f13f96aeb534843
Geerd Kakes <geerd@kakes.net>
2022-04-22 22:41:24 +0200

    corrected log output

--- run_ping.sh
+++ run_ping.sh
@@ -98,4 +98,4 @@ sleep 3
 # retrieve device logfile 
 ##########################################
 scp ${userid_device}@${deviceIP}:${data_dir_device}/${session_id}/device${testID}_ping_${testdate}.log  ${data_dir_server}/${session_id}/
-echo "${scriptname}: stored nuttcp output at ${data_dir_server}/${session_id}/device${testID}_ping_${testdate}.log"
+echo "${scriptname}: stored ping output at ${data_dir_server}/${session_id}/device${testID}_ping_${testdate}.log"
